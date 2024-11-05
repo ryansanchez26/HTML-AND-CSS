d0c1000bc6d61abf23d831bc37e86fb4d1daaf47
RYAN SANCHEZ <sanchezryan0926@gmail.com>
2024-11-05 12:50:40 -0600

    HTML

---
+++ Basic_HTML_and JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+		
+alert("Hello World, This is a basic Javascript")
+
+function My_first_function() {
+		var String = "Kiss me, I'm Irish";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML = result;
+		}
+		
